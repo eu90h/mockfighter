3aec57d0c8ee85ace756af389e06d946540ddc89
Stefan <euler90h@gmail.com>
2015-12-25 02:38:57 -0500

    added heartbeat tests

--- server-test.rkt
+++ server-test.rkt
@@ -1,6 +1,7 @@
 #lang racket
 (require web-server/servlet
          web-server/servlet-env
+         rackunit
          "utils.rkt"
          "server.rkt"
          stockfighter-api)
@@ -11,11 +12,15 @@
 (send sf set-gm-endpoint "127.0.0.1")
 (send sf set-port 8000)
 (send sf ssl-off)
+(check-true (send sf is-api-up?))
 
 (define game-data (send sf new-instance "test"))
 (define venue (hash-ref game-data 'venue))
 (define stock (hash-ref game-data `symbol))
 (define account (hash-ref game-data 'account))
+
+(check-false (send sf is-venue-up? "blah"))
+(check-true (send sf is-venue-up? venue))
 
 (define buy-data (send sf post-order account venue stock 1000 40 "buy" "limit"))
 (define sell-data (send sf post-order account venue stock 1050 40 "sell" "limit"))
